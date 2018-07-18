a10f24e478012fc4012bea3ebbdbe5e872f120a2
Max Twardowski <maxio1998@gmail.com>
2018-07-19 00:37:01 +0200

    Added Login form fundamentals

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, url_for
-from forms import RegistrationForm
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '382b514f430d789f42c196072f3bbe78'
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm();
-    return render_template('registration.html', title='Sign Up', form=form)
+    form = RegistrationForm()
+    #submission handling to be done 
+    return render_template('registration.html', title='Create Account', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email')
     password = PasswordField('Password')
     password_confirm = PasswordField('Confirm Password')
     submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email')
+    password = PasswordField('Password')
+    staylogged = BooleanField('Stay logged in')
+    submit = SubmitField('Login')
