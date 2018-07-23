21f014b36ac44aa8dfe8b75923e30ee5699668c4
maxtwardowski <twardowskimax@gmail.com>
2018-07-23 22:57:49 +0200

    Added SQLite3 database fundamentals with SQLAlchemy. Created first prototypes of User and Note models

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template, url_for
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '382b514f430d789f42c196072f3bbe78'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    date_join = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    notes = db.relationship('Note', backref='author', lazy=True)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_create = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_edit = db.Column(db.DateTime, nullable=True, default=None)
+
 
 @app.route("/", methods=['GET', 'POST'])
 def register():
