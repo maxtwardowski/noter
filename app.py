828c06c7872582c24707456f3c170896d88ff687
maxtwardowski <twardowskimax@gmail.com>
2018-07-23 23:20:50 +0200

    Created the database files, added some test entries and created __repr__ method for the User class for readability

--- app.py
+++ app.py
@@ -16,7 +16,11 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     date_join = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    notes = db.relationship('Note', backref='author', lazy=True)
+    #notes = db.relationship('Note', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.email}', '{self.date_join}', '{self.id}')"
+
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
