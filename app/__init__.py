cc4f03a2e6260430155d9213088f7311069010e1
Max Twardowski <twardowskimax@gmail.com>
2018-07-30 22:10:19 +0200

    Pages load but login feature crashes

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '382b514f430d789f42c196072f3bbe78'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from app import routes
