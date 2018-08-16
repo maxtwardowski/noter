4c4c42963f4d161f2001de2d3b47002cdc77311c
Max Twardowski <twardowskimax@gmail.com>
2018-08-16 14:20:32 +0200

    Added action types and other boilerplate redux stuff

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './static/index.css';
 
 const app = document.getElementById('root');
-ReactDOM.render(<App />, app);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  app
+);
 registerServiceWorker();
