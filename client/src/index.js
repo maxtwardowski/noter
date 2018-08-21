f23bedd9ff2f685e13402ba565c015f6fe34a570
Max Twardowski <twardowskimax@gmail.com>
2018-08-21 22:41:18 +0200

    Added redux-persist not to lose the store after refresh

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-import { Provider } from 'react-redux';
-import store from './store';
-
-import './static/index.css';
-
-window.store = store;
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { Provider } from 'react-redux'
+import { store, persistor } from './store'
+import './static/index.css'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const app = document.getElementById('root');
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   app
 );
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,21 @@
-import { createStore, applyMiddleware } from "redux";
-import reduxThunk from "redux-thunk";
-import reducer from '../reducers';
+import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
-const store = createStoreWithMiddleware(reducer);
+import reducer from '../reducers'
 
-export default store;
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composeWithDevTools(applyMiddleware()),
+);
+
+export const persistor = persistStore(store);
